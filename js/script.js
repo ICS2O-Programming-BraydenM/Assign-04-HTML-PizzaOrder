6fc03298794a3f4ca523f31d5ff99a163052479b
Brayden MacMillan <99274619+BraydenMacMillan7@users.noreply.github.com>
2022-05-09 18:30:16 +0000

    fixed a few errors

--- js/script.js
+++ js/script.js
@@ -12,31 +12,31 @@ function displayCost() {
     const TOPPING_PRICE = 0.50;
 
     //get user input
-   let pizzaSize = document.getElementById('size').value
-   let crustType = document.getElemetById('crust').value
+   let pizzaSize = document.getElementById('size').value;
+   let crustType = document.getElementById('crust').value;
 
   // initializing variable for pizza
-   let baseCost = 0
+   let baseCost = 0;
 
   // finding the cost and size of the pizza size before toppings and side of wine
-  if ((pizzaSize == "small") && (crustType == "flatbread")) {
-    baseCost = 7.79;
-  }
-  else if ((pizzaSize == "small") && (crustType == "stuffed")) {
-    baseCost = 10.79;
-  }
-  else if ((pizzaSize == "medium") && (crustType == "flatbread")) {
-    baseCost = 14.25;
-  }
-  else if ((pizzaSize == "medium") && (crustType == "stuffed")) {
-    baseCost = 17.25;
-  }
-  else if ((pizzaSize == "large") && (crustType == "flatbread")) {
-    baseCost = 19.87;
-  }
-  else if ((pizzaSize == "large") && (crustType == "stuffed")) {
-    baseCost = 22.87;
-  }
+    if ((pizzaSize == "small") && (crustType == "flatbread")) {
+      baseCost = 7.79;
+    }
+    else if ((pizzaSize == "small") && (crustType == "stuffed")) {
+      baseCost = 10.79;
+    }
+    else if ((pizzaSize == "medium") && (crustType == "flatbread")) {
+      baseCost = 14.25;
+    }
+    else if ((pizzaSize == "medium") && (crustType == "stuffed")) {
+      baseCost = 17.25;
+    }
+    else if ((pizzaSize == "large") && (crustType == "flatbread")) {
+      baseCost = 19.87;
+    }
+    else if ((pizzaSize == "large") && (crustType == "stuffed")) {
+      baseCost = 22.87;
+    }
 
   // check all check boxes to see how many there are, from https://www.techiedelight.com/count-number-check-boxes-javascript/
  
@@ -52,17 +52,16 @@ function displayCost() {
   // if statement to see what radio button was clicked and to find cost of wine
   if(document.getElementById('option-1').checked) {
   //No thank you radio button is clicked
-    wineCost = 0;
-  }else if(document.getElementById('option-2').checked) {
+    wineCost = 0 
+  }
+  else if(document.getElementById('option-2').checked) {
   //Yes please radio button is clicked
-    wineCost = 27;
+    wineCost = 27.00;
 }
 
   // calculate subtotal
   let subTotal = baseCost + toppingCost + wineCost
   let tax = subTotal * HST 
   let total = subTotal + tax
-
-  // display results back to user
-  document.getElementById('display-results').innerHTMl = subTotal
+  
 }
