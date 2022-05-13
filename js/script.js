cf46441abf21b65cbbc00d0f6ce0a4c1b0394d6d
Brayden MacMillan <99274619+BraydenMacMillan7@users.noreply.github.com>
2022-05-13 23:49:56 +0000

    added extra constants

--- js/script.js
+++ js/script.js
@@ -8,37 +8,49 @@
 
 function displayCost() {
     // display constants
+    // constant tax price
     const HST = 0.13;
+    // constant topping price
     const TOPPING_PRICE = 0.50;
+    // costs for each combination of pizza size and crust type
+    const SMALL_FLATBREAD = 7.79;
+    const SMALL_STUFFED = 10.79;
+    const MED_FLATBREAD = 14.25;
+    const MED_STUFFED = 17.25;
+    const LARGE_FLATBREAD = 19.87;
+    const LARGE_STUFFED = 22.87;
 
-    //get user input
+    //get user input for pizza size and crust type
    let pizzaSize = document.getElementById('size').value;
    let crustType = document.getElementById('crust').value;
 
   // initializing variable for pizza
    let baseCost = 0;
 
-  // finding the cost and size of the pizza size before toppings and side of wine
+   //initializing variable for extra side of wine 
+  let wineCost = 0;
+
+  // finding the cost of size and type of crust for the pizza before toppings and side of wine
     if ((pizzaSize == "small") && (crustType == "flatbread")) {
-      baseCost = 7.79;
+      baseCost = SMALL_FLATBREAD;
     }
     else if ((pizzaSize == "small") && (crustType == "stuffed")) {
-      baseCost = 10.79;
+      baseCost = SMALL_STUFFED;
     }
     else if ((pizzaSize == "medium") && (crustType == "flatbread")) {
-      baseCost = 14.25;
+      baseCost = MED_FLATBREAD;
     }
     else if ((pizzaSize == "medium") && (crustType == "stuffed")) {
-      baseCost = 17.25;
+      baseCost = MED_STUFFED;
     }
     else if ((pizzaSize == "large") && (crustType == "flatbread")) {
-      baseCost = 19.87;
+      baseCost = LARGE_FLATBREAD;
     }
     else if ((pizzaSize == "large") && (crustType == "stuffed")) {
-      baseCost = 22.87;
+      baseCost = LARGE_STUFFED;
     }
   
-  // check all check boxes to see how many there are, from https://www.techiedelight.com/count-number-check-boxes-javascript/
+  // check all checkboxes to see how many there are, from https://www.techiedelight.com/count-number-check-boxes-javascript/
  
     let numberOfCheckboxes = document.querySelectorAll('input[type="checkbox"]:checked');
 
@@ -46,20 +58,17 @@ function displayCost() {
 
   let toppingCost = numberOfToppings * TOPPING_PRICE
 
-  //initializing variables
-  let wineCost = 0;
-    
   // if statement to see what radio button was clicked and to find cost of wine
   if(document.getElementById('option-1').checked) {
   //No thank you radio button is clicked
-    wineCost = 0 
+    wineCost = 0;
   }
   else if(document.getElementById('option-2').checked) {
   //Yes please radio button is clicked
     wineCost = 27.00;
 }
 
-  // calculate subtotal
+  // calculate subtotal, tax and total
   let subTotal = baseCost + toppingCost + wineCost
   let tax = subTotal * HST 
   let total = subTotal + tax
